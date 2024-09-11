bdf747ad4533079b941973ad9da781a74b194902
Camilo Escar <camiloescar1995@gmail.com>
2024-09-11 15:08:36 -0300

    trabajo hecho en local

---
+++ src/components/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { Alert } from 'flowbite-svelte';
+    import {APP_STATUS, appStatus} from '../pages/store'
+  import StepUpload from './StepUpload.svelte';
+  import StepLoading from './StepLoading.svelte';
+  import StepChat from './StepChat.svelte';
+  </script>
+
+{#if $appStatus === APP_STATUS.INIT}
+<StepUpload />
+{:else if $appStatus === APP_STATUS.LOADING}
+<StepLoading/>
+
+{:else if $appStatus === APP_STATUS.ERROR}
+<Alert>
+    <span class="font-medium">algo malo paso!</span>
+    hay un error en la aplicacion
+</Alert>
+{:else if $appStatus === APP_STATUS.CHAT_MODE}
+<StepChat />
+{:else}
+<Alert>
+    <span class="font-medium">accion no reconocida</span>
+    este estado de la aplicacion no esta reconocida
+</Alert>
+{/if}
+
